9cb78beb1885159b75cf6968ab8ddabcfa7eee71
yufoxda <mysubaccoun1@gmail.com>
2026-02-03 12:46:57 +0900

    fix:canonical link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "tuat electone",
   description: "東京農工大学エレクトーンサークル 音風",
+  alternates: {
+    canonical: 'https://www.ompoo.org',
+  },
 };
 
 export default function RootLayout({
